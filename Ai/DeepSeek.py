772d7fe7830b1d07f12ec15df22105674c2c32ca
Chronos <merk235@gmail.com>
2025-03-29 20:17:41 +0500

    LAW-38 add DeepSeek

---
+++ Ai/DeepSeek.py
@@ -0,0 +1,14 @@
+from openai import OpenAI
+
+client = OpenAI(api_key="sk-2764177307db4025aaf88bee1cf0d9d5", base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
